a855d3c6a983d7404c86f6a0ce3f2660d71458dd
Selina <tunth301201@gmail.com>
2023-06-10 17:42:41 +0700

    hide db

--- db.ts
+++ db.ts
@@ -1,6 +1,6 @@
 import mongoose, { ConnectOptions } from "mongoose";
 
-mongoose.connect('mongodb+srv://group1940:19401940@cluster0.txsa0qr.mongodb.net/racingf1?retryWrites=true&w=majority', {
+mongoose.connect('', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 } as ConnectOptions);
